de6aa17b95e2a72455cfac86020d46e3970d2b2c
Omar Espinoza <aomar.espinoza@gmail.com>
2022-08-12 00:24:54 +0000

    updting github link

--- app.py
+++ app.py
@@ -14,15 +14,11 @@ color1='#92A5E8'
 color2='#8E44AD'
 color3='#FFC300'
 sourceurl = 'https://www.kaggle.com/c/titanic'
-githublink = 'https://github.com/plotly-dash-apps/304-titanic-dropdown'
+githublink = 'https://github.com/satrivaicci/304-titanic-dropdown'
 
 
 ###### Import a dataframe #######
-df = pd.read_csv("https://raw.git.generalassemb.ly/intuit-ds-15/05-cleaning-combining-data/master/data/drinks.csv?token=AAAK2H43E5OWLGL6F7TI2OLC7RDXQ", keep_default_na=False)
-# df = pd.read_csv("https://raw.githubusercontent.com/austinlasseter/plotly_dash_tutorial/master/00%20resources/titanic.csv")
-# df['Female']=df['Sex'].map({'male':0, 'female':1})
-# df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
-# variables_list=['Survived', 'Female', 'Fare', 'Age']
+df = pd.read_csv("https://raw.git.generalassemb.ly/intuit-ds-15/05-cleaning-combining-data/master/data/drinks.csv?token=AAAK2H43E5OWLGL6F7TI2OLC7RDXQ", keep_default_na=False) # keep_default_na=False solves the problem of NA continent being read as NaN
 df['continent_name'] = df['continent'].map({ 'NA':'North America', 'SA':'South America', 'EU':'Europe', 'AF':'Africa', 'OC': 'Oceania', 'AS': 'Asia' })
 variables_list=pd.unique(df['continent_name'])
 
